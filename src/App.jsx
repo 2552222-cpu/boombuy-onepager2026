464e94f454c101b88eb8d818a5daaf4bc39e825a
אוריאל מצליח <contact@boombuy.co.il>
2026-03-22 18:05:46 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import EmployeesLanding from './pages/EmployeesLanding';
+import AdminDashboard from './pages/AdminDashboard';
 // Add page imports here
 
 const AuthenticatedApp = () => {
@@ -35,6 +36,7 @@ const AuthenticatedApp = () => {
   return (
     <Routes>
       <Route path="/" element={<EmployeesLanding />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
